62b91dd6b52555841c49939550a04eba61737bef
DhananjayaKumara99 <it21184208@my.sliit.lk>
2024-02-01 20:06:42 +0530

    change site name

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo} alt="Shopper" />
-            <p>SHOPPER</p>
+            <p>VISTA</p>
         </div>
         <ul className="nav_menu">
             <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>  {menu==="shop"?<hr/>:<></>}</li>
